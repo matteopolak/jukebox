172dfe9b947b0435a51c736a7f457a14ae6206bc
matteopolak <matteopolak@hotmail.com>
2022-08-31 19:04:43 -0400

    add a speech buffer to make it more seamless

--- src/api/tts.ts
+++ src/api/tts.ts
@@ -1,30 +1,55 @@
 import { Readable } from 'node:stream';
-import axios from 'axios';
-import { Language } from '../typings/common.js';
+import axios, { AxiosResponse } from 'axios';
+import { Language, Option } from '../typings/common.js';
 
-const SPLIT_REGEX = /[^\s\uFEFF\xA0!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~]{1,200}/g;
+const SPLIT_REGEX = /.{1,200}(?=$|\s)/g;
 
 function* splitText(text: string, language: Language) {
-	const matches = text.matchAll(SPLIT_REGEX);
+	const matches = text.replace(/[\r\n]/g, ' ').matchAll(SPLIT_REGEX);
+
+	let index = 0;
 
 	for (const [match] of matches) {
 		yield `https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(
 			match
-		)}&tl=${language}&total=1&idx=0&textlen=${
-			text.length
+		)}&tl=${language}&idx=${index++}&total=${index}&textlen=${
+			match.length
 		}&client=tw-ob&prev=input&ttsspeed=1`;
 	}
 }
 
 async function* textToAudioGenerator(text: string, language: Language = 'en') {
 	const urls = splitText(text, language);
+	let next: Option<Promise<AxiosResponse<Readable, any>>> = null;
 
 	for (const url of urls) {
-		const stream = await axios.get<Readable>(url, {
+		if (next === null) {
+			const url = urls.next().value;
+
+			if (url) {
+				next = axios.get<Readable>(url, {
+					responseType: 'stream',
+				});
+			} else {
+				break;
+			}
+		}
+
+		const streamPromise = next;
+
+		next = axios.get<Readable>(url, {
 			responseType: 'stream',
 		});
 
+		const stream = await streamPromise;
+
 		for await (const chunk of stream.data) {
+			yield chunk as Buffer;
+		}
+	}
+
+	if (next) {
+		for await (const chunk of (await next).data) {
 			yield chunk as Buffer;
 		}
 	}
