b62425fd0593a4c31e1138fc55eeaef48aeb2f1e
matteopolak <matteopolak@hotmail.com>
2022-08-31 14:24:19 -0400

    feat: use generators for splitting text, remove 'google-tts-api' dep

--- src/api/tts.ts
+++ src/api/tts.ts
@@ -1,23 +1,31 @@
-import { getAllAudioUrls } from 'google-tts-api';
 import { Readable } from 'node:stream';
 import axios from 'axios';
 import { Language } from '../typings/common.js';
 
-async function* textToAudioGenerator(text: string, language: Language = 'en') {
-	const results = getAllAudioUrls(text, {
-		lang: language,
-		slow: false,
-		host: 'https://translate.google.com',
-		splitPunct: ',.?',
-	});
+const SPLIT_REGEX = /[^\s\uFEFF\xA0!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~]{1,200}/g;
 
-	for (const result of results) {
-		const stream = await axios.get(result.url, {
+function* splitText(text: string, language: Language) {
+	const matches = text.matchAll(SPLIT_REGEX);
+
+	for (const [match] of matches) {
+		yield `https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(
+			text
+		)}&tl=${language}&total=1&idx=0&textlen=${
+			text.length
+		}&client=tw-ob&prev=input&ttsspeed=1`;
+	}
+}
+
+async function* textToAudioGenerator(text: string, language: Language = 'en') {
+	const urls = splitText(text, language);
+
+	for (const url of urls) {
+		const stream = await axios.get<Readable>(url, {
 			responseType: 'stream',
 		});
 
 		for await (const chunk of stream.data) {
-			yield chunk;
+			yield chunk as Buffer;
 		}
 	}
 }
