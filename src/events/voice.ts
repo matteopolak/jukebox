291fb95d4232b061db78e4933e948a0bb1b5291b
matteopolak <matteopolak@hotmail.com>
2022-12-10 22:06:10 -0500

    fix: voice activity check

--- src/events/voice.ts
+++ src/events/voice.ts
@@ -1,17 +1,27 @@
-import { Client } from 'discord.js';
+import { Client, VoiceState } from 'discord.js';
 
 import Connection from '@/structures/connection';
+import { Option } from '@/typings/common';
 import { MAIN_CLIENT } from '@/util/worker';
 
 export function register(client: Client) {
-	client.on('voiceStateUpdate', async (oldState, newState) => {
-		if (oldState.channelId === newState.channelId || newState !== null || oldState === null) return;
+	client.on('voiceStateUpdate', async (oldState: Option<VoiceState>, newState: Option<VoiceState>) => {
+		if (oldState?.channelId === newState?.channelId) return;
 
-		const connection = await Connection.getOrCreate(oldState);
-		if (!connection) return;
+		// if there is only one person in the channel and it's the bot, pause the connection
+		if (oldState && oldState.channel?.members.size === 1 && oldState.channel?.members.has(MAIN_CLIENT.user!.id)) {
+			const connection = await Connection.getOrCreate(oldState);
+			if (!connection) return;
 
-		if (oldState.channel?.members.size === 1 && oldState.channel?.members.has(MAIN_CLIENT.user!.id)) {
-			return connection.destroy();
+			return connection.pause();
+		}
+
+		// if there are 2 people in the channel and it's the bot and the other person, resume the connection
+		if (newState && newState.channel?.members.size === 2 && newState.channel?.members.has(MAIN_CLIENT.user!.id)) {
+			const connection = await Connection.getOrCreate(newState);
+			if (!connection) return;
+
+			return connection.resume();
 		}
 	});
 }
