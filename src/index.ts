9d70fc6b8b0f6a5d769bb9b3902934a929cf2523
matteopolak <matteopolak@hotmail.com>
2022-12-10 21:52:29 -0500

    fix: use the voice activity listener

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import * as guild from '@/events/guild';
 import * as interaction from '@/events/interaction';
 import * as login from '@/events/login';
 import * as message from '@/events/message';
+import * as voice from '@/events/voice';
 
 import { MAIN_CLIENT } from './util/worker';
 
@@ -13,3 +14,4 @@ guild.register(MAIN_CLIENT);
 interaction.register(MAIN_CLIENT);
 login.register(MAIN_CLIENT);
 message.register(MAIN_CLIENT);
+voice.register(MAIN_CLIENT);
