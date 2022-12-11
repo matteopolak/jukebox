d2dc12dc4a9a5b95cf08be4ed1d974cb36766f90
matteopolak <matteopolak@hotmail.com>
2022-12-10 21:54:22 -0500

    fix: voice listener

--- src/events/voice.ts
+++ src/events/voice.ts
@@ -1,6 +1,7 @@
 import { Client } from 'discord.js';
 
 import Connection from '@/structures/connection';
+import { MAIN_CLIENT } from '@/util/worker';
 
 export function register(client: Client) {
 	client.on('voiceStateUpdate', async (oldState, newState) => {
@@ -9,7 +10,7 @@ export function register(client: Client) {
 		const connection = await Connection.getOrCreate(oldState);
 		if (!connection) return;
 
-		if (oldState.channel?.members.size === 0) {
+		if (oldState.channel?.members.size === 1 && oldState.channel?.members.has(MAIN_CLIENT.user!.id)) {
 			return connection.destroy();
 		}
 	});
